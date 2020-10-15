23d1a44fdeec6492e601c2a7b4f61b7f25633b73
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-15 10:00:17 +0530

    merged code for hello demo not this gorgon

--- src/demo/DemoClass.java
+++ src/demo/DemoClass.java
@@ -4,11 +4,11 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("Hello demo not this gorgon ");
-=======
+
 		System.out.println("Hello demogorgon");
->>>>>>> 4030c4a698db21bd98a1dd5818155eff701d6cc0
+
 
 	}
 
